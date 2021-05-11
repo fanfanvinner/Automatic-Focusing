756f96f5ac5d98b3045a933d040de35292798bce
jerryweihuajing <jerryweihuajing@126.com>
2021-05-11 11:50:38 +0800

    try to get access to OD

---
+++ python/script_focused_point.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 11:26:03 2021
+
+@author: Wei Huajing
+@company: KAMERAWERK
+@e-mail: jerryweihuajing@126.com
+
+@title: script-Focused Point based on DoF
+"""
+
